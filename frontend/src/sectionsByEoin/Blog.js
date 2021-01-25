213845f8930eeb7dd67583cbc99078453dba59fe
eoinlynchcodes <eoin95lynch@gmail.com>
2021-01-25 08:19:42 +0100

    stockists done

--- frontend/src/sectionsByEoin/Blog.js
+++ frontend/src/sectionsByEoin/Blog.js
@@ -9,32 +9,33 @@ function Blog() {
   return (
       <section className="aboutContainer">
         <section className="principlesbox">
-          <p>We are proud to be stocked in the following selected stores.
+          <p className="centerThis">We are proud to be stocked in the following selected stores.
+            <br/>
+            Together we support our local food community.</p>
             <br/>
             <br/>
-            Together we support our local food community</p>
           <div id="" className="">
-            <p>Hereford and More. Tullamore, Offaly</p>
+            <p>Hereford and More - Tullamore, Offaly</p>
             <hr />
-            <p>Foxes Fruit and Veg. Mullingar, Westmeath</p>
+            <p>Foxes Fruit and Veg - Mullingar, Westmeath</p>
             <hr />
-            <p>Nuts and Grains. Mullingar, Westmeath</p>
+            <p>Nuts and Grains - Mullingar, Westmeath</p>
             <hr />
-            <p>Lowe and Co. Athlone, Westmeath</p>
+            <p>Lowe and Co. - Athlone, Westmeath</p>
             <hr />
-            <p>Marlowe and Co. Dublin 8</p>
+            <p>Marlowe and Co. - Dublin 8</p>
             <hr />
-            <p>Lennox Street Grocer. Portobello, Dublin</p>
+            <p>Lennox Street Grocer - Portobello, Dublin</p>
             <hr />
-            <p>Barrow Market. Dublin 4.</p>
+            <p>Barrow Market - Dublin 4</p>
             <hr/>
-            <p>Mamo Restaurant. Howth, Dublin</p>
+            <p>Mamo Restaurant - Howth, Dublin</p>
             <hr />
-            <p>Margadh Specialty Shop. Howth, Dublin</p>
+            <p>Margadh Specialty Shop - Howth, Dublin</p>
             <hr />
-            <p>The Dublin Food Co-Op. Kilmainham, Dublin</p>
+            <p>The Dublin Food Co-Op. - Kilmainham, Dublin</p>
             <hr />
-            <p>The Fumbally Shop. Dublin 8.</p>
+            <p>The Fumbally Shop - Dublin 8</p>
             <hr/>
           </div>
         </section>
