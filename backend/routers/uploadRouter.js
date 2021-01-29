47d8b33da119edaed60c04d27e271487abff2d53
eoinlynchcodes <eoin95lynch@gmail.com>
2021-01-29 13:19:14 +0100

    Go

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -25,7 +25,7 @@ aws.config.update({
 const s3 = new aws.S3();
 const storageS3 = multerS3({
   s3,
-  bucket: 'timberandbarkmulch',
+  bucket: 'four-hands-food-studio',
   acl: 'public-read',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   key(req, file, cb) {
